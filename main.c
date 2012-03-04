019c4c7b90bcfa4f5fe5f5809b687615b4447f11
Thomas Battermann <git@thomasba.de>
2012-03-04 07:30:58 +0100

    Bugfix: outfile given -> overlay used

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
 	/* Read the command line parameters */
 	readOptions(argc,argv,&o);
 	/* feed the functions */
-	if(o.outfile == NULL) {
+	if(o.overlay == NULL) {
 		printf("default\n");
 		newStandard(&o);
 	}else{
